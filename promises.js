cd669f7a4e5035910be9b6b6650512c8233d1283
Muhammad waryal <waryalbmemon@gmail.com>
2026-01-22 10:56:39 +0500

    Promises

---
+++ promises.js
@@ -0,0 +1,14 @@
+let pr = new Promise((resolve,reject)=>{
+setTimeout(()=>{
+          let number = Math.floor(Math.random() * 10);
+          if(number > 5){
+                    resolve("Resolve with "+number);
+          }
+          else{
+                    reject("REject with" +number);
+          }
+},2000)
+})
+
+pr.then(data =>console.log(data))
+pr.catch(error=> console.log(error));
